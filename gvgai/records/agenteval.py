57273d9b572396fbe6460126efd499d65b3f512a
Shuki <jo.ma@posteo.de>
2015-11-16 09:28:35 +0100

    python skript now returns best bot for each game/level-pair

--- gvgai/records/agenteval.py
+++ gvgai/records/agenteval.py
@@ -5,20 +5,29 @@ import os
 
 def evaluate():
 	# for example: for each game and level find the best bot
-	#1. make dict struct:
 	data = readData()
+	# for each game and level save the bot that performed best in games[game][level]
 	games = {}
-	for i in [0,1,2,3,4,5,6,7,8,9]:
+	for i in [0,1,2,3,4,5,6,7,8,9]:  # create empty dict struct
 		games[i] = {}
 		for j in [0,1,2,3,4]:
-			games[i][j] = None
+			games[i][j] = [None, -10000] # [bot, score]
+	# go through bots and find best run
 	for bot in data:
 		for game in data[bot]:
 			for level in data[bot][game]:
+				# find best run
 				maximum = -10000
 				for run in data[bot][game][level]:
-					if data[bot][game][level][run]
-		scores = []
+					if data[bot][game][level][run][2] > maximum:
+						maximum = data[bot][game][level][run][2]
+				# if best run is better than any other bots...
+				print(game,level)
+				if maximum > games[game][level][1]:
+					games[game][level][0] = bot
+					games[game][level][1] = maximum
+	for n in games:
+		print(games[n])
 		
 	
 """
@@ -43,10 +52,14 @@ def readData():
 
 	# create data structure
 	data = {}
-	#[bot, [game, [level, [#ofRepitition, (boolWin, [score], finalScore)]]]]
+	#{bot: {game: {level: {#ofRepitition: [win, [score], finalScore]}}}}
 	for name in filenames:
 		rx = re.split('_|\.', name)
-		# add bot, game, level, etx. initialize all dicts:
+		# turn strings from re to int
+		for d in range(len(rx)):
+			if d not in [0, 1, 3, 6]: # those actually arent numbers
+				rx[d] = int(rx[d])
+		# add bot, game, level, run. initialize all dicts:
 		if rx[0] not in data:
 			data[rx[0]] = {}
 		if rx[2] not in data[rx[0]]:
@@ -54,7 +67,7 @@ def readData():
 		if rx[4] not in data[rx[0]][rx[2]]:
 			data[rx[0]][rx[2]][rx[4]] = {}
 		if rx[5] not in data[rx[0]][rx[2]][rx[4]]:
-			data[rx[0]][rx[2]][rx[4]][rx[5]] = [None, [], None] 
+			data[rx[0]][rx[2]][rx[4]][rx[5]] = [None, [], None] #[win, [score], final]
 		# save stuff
 		f = open(name, 'r')
 		for line in f:
@@ -62,11 +75,11 @@ def readData():
 			ry = re.split(',', line)
 			if len(ry) == 3:
 				# write score
-				data[rx[0]][rx[2]][rx[4]][rx[5]][1].append(ry[1])
+				data[rx[0]][rx[2]][rx[4]][rx[5]][1].append(float(ry[1]))
 			if len(ry) == 1 and re.match('didWin: .*', ry[0]):
-				data[rx[0]][rx[2]][rx[4]][rx[5]][0] = re.sub('didWin: ', '', ry[0])
+				data[rx[0]][rx[2]][rx[4]][rx[5]][0] = int(re.sub('didWin: ', '', ry[0]))
 			if (len(ry) == 1) and (re.match('finalScore: .*', ry[0])):
-				data[rx[0]][rx[2]][rx[4]][rx[5]][2] = re.sub('finalScore: ', '', ry[0])
+				data[rx[0]][rx[2]][rx[4]][rx[5]][2] = float(re.sub('finalScore: ', '', ry[0]))
 	return data
 
 
